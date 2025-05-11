31af32a82d5fde70be5edd2c8b34c27df67bf145
Jeri Fan <jeri.uwlu@gmail.com>
2025-05-11 12:54:45 -0400

    clean up code structure

---
+++ transaction.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <fstream>
+#include <mutex>
+#include <memory>
+#include <chrono>
+#include <thread>
+#include <atomic>
+
+using namespace std;
+
+class Transaction {
+
+};
