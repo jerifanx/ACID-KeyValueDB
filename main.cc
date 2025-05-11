facc501e47d292648eb1ae3a08978c5131c068cd
Jeri Fan <jeri.uwlu@gmail.com>
2025-05-10 23:30:57 -0400

    first changes

--- main.cc
+++ main.cc
@@ -9,4 +9,44 @@
 #include <thread>
 #include <atomic>
 
+using namespace std;
 class Transaction;
+
+class KeyValueStore {
+    private:
+        unordered_map<string, string> data;
+        mutex m;
+        ofstream logFile;
+        void addToLog(const string& operation) {
+            if (logFile.is_open()) {
+                logFile << operation << endl;
+                logFile.flush();
+            } 
+        }
+    
+    public:
+        KeyValueStore() {
+            logFile.open("kv_store.log", ios::app);
+            if (!logFile.is_open()) {
+                cerr << "Log file cannot be opened, durability at risk." << endl;
+            }
+        }
+
+        ~KeyValueStore() {
+            if (logFile.is_open()) {
+                logFile.close();
+            }
+        }
+
+        bool put(const string& key, const string& value) {
+
+        }
+
+        bool get(){
+
+        }
+        
+        bool remove() {
+
+        }
+};
